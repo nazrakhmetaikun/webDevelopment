6e44df4fdf5691cd0a745fdf95daf92854a4525f
nazrakhmetaikun <62601592+nazrakhmetaikun@users.noreply.github.com>
2021-02-24 14:32:19 +0600

    lab4

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,73 @@
+export const products = [
+  {
+    id: 1,
+    name: 'TruSkin Vitamin C Serum for Face with Hyaluronic Acid, Vitamin E, Witch Hazel, 1 fl oz',
+    price: 19.99,
+    description: 'ANTIOXIDANT FACE SERUM - Vitamin C blends with Botanical Hyaluronic Acid, Vitamin E, Witch Hazel, and Jojoba Oil to promote your skins response to signs of aging like brightness, wrinkles, & dark spots',
+    image:'https://i.otzovik.com/objects/b/1690000/1681517.png',
+    link:'https://www.amazon.com/TruSkin-Naturals-Vitamin-Topical-Hyaluronic/dp/B01M4MCUAF/ref=lp_16225006011_1_8'
+  },
+  {
+    id: 2,
+    name: 'TruSkin Vitamin C Moisturizer Face, Neck & Décolleté Cream for All Skin Types with Vitamin B5 and Green Tea, 2 fl oz',
+    price: 15.99,
+    description: 'VITAMIN C CREAM WITH ANTIOXIDANTS - Daily facial lotion with ingredients designed to promote your skins natural health. Ultra-effective and lightweight in order to be enjoyed by all skin types.',
+    link:'https://www.amazon.com/dp/B0166W9GMI/ref=sspa_dk_detail_5?psc=1&pd_rd_i=B0166W9GMI&pd_rd_w=8oOCJ&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=udMXL&pf_rd_r=DH62WDVRK6SNXV3G404X&pd_rd_r=2fc17151-72d8-4e17-82a9-43b38f8eac2e&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyUE9COFdKSFVRVEkmZW5jcnlwdGVkSWQ9QTA2NzYwNDczRTBVRkFPME1JSlRHJmVuY3J5cHRlZEFkSWQ9QTA0NDkyODExQjk1UVJWNEkzTjQyJndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==',
+    image:'https://images-na.ssl-images-amazon.com/images/I/71MhQUg6UzL._SL1500_.jpg'
+  },
+  {
+    id: 3,
+    name: 'Neck Firming Cream, Anti-Aging Neck Cream for Skin Tightening, Marine Collagen Cream with Peptides for Face and Neck, Retinol Serum for Neck Skin Firming - SkinPro',
+    price: 27.97,
+    link:'https://www.amazon.com/dp/B01J4GVVTO/ref=sspa_dk_detail_2?psc=1&pd_rd_i=B01J4GVVTO&pd_rd_w=JB0Ca&pf_rd_p=cbc856ed-1371-4f23-b89d-d3fb30edf66d&pd_rd_wg=rJYAj&pf_rd_r=3DE619XTHHWK816SQD4Y&pd_rd_r=bc0605a4-e545-446b-a609-86e5c7deec25&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExVzdNRlBLTFVMU09FJmVuY3J5cHRlZElkPUEwNDU0MTkyMVhMMU00TE44SEIwQSZlbmNyeXB0ZWRBZElkPUEwOTQxNjcyMlQ0OUJXWFFGVDYxRSZ3aWRnZXROYW1lPXNwX2RldGFpbF90aGVtYXRpYyZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+    description: 'NO MORE SAGGY SKIN: With a balanced blend of peptides and vitamin A from retinol, our retinol creams tighten the delicate skin on your neck. It is designed to be a multi-action formula to promotes collagen production and restores skin elasticity for smoother, firmer skin.',
+    image:'https://images-na.ssl-images-amazon.com/images/I/618C7CkotYL._SL1500_.jpg'
+  },
+  {
+    id: 4,
+    name: 'Neck Firming Cream - Anti Wrinkle Cream - Made in USA - Saggy Neck TIghtener & Double Chin Reducer Cream - Collagen & Retinol Skin Tightening Cream - Anti Aging Moisturizer for Neck & Décolleté',
+    price: 26.00,
+    description: 'Pure anti-aging neck cream - Pure Sciences created the best solution for saggy neck & double chin. Our neck and face tightening & lifting cream with natural nourishing oils, retinol, and collagen effectively reduces wrinkles, firms flabby neck skin, and lifts jawline. Get a smooth neck & décolleté and forget about imperfections!',
+    link:'https://www.amazon.com/dp/B08GL3G1Y9/ref=sspa_dk_detail_1?psc=1&pd_rd_i=B08GL3G1Y9&pd_rd_w=tPiAZ&pf_rd_p=cbc856ed-1371-4f23-b89d-d3fb30edf66d&pd_rd_wg=939Y5&pf_rd_r=FHKZTFAVVA3C24TV3MEB&pd_rd_r=c202d095-0765-45fe-8045-68a2dc390af8&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyRFhJVEo4QUJJMUVGJmVuY3J5cHRlZElkPUEwNDE2MDc2M0VGTFg3Qk81VDJYRSZlbmNyeXB0ZWRBZElkPUEwMjEzMDExMjhBWkQ5WkdETUJUNyZ3aWRnZXROYW1lPXNwX2RldGFpbF90aGVtYXRpYyZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+    image:'https://images-na.ssl-images-amazon.com/images/I/71mo-OkzGWL._SL1500_.jpg'
+  },
+  {
+    id: 5,
+    name: 'Face Moisturizer for Men - Anti Wrinkle Day & Night Face Cream - Made in USA - Сollagen, Retinol, Hyaluronic Acid, Vitamin E - Organic Anti Aging Face Lotion to Hydrate, Firm and Reduce Fine Lin',
+    price: 21.49,
+    description: 'Mens Face Moisturizer - Neubeautics Collagen Cream for Men elevates your daily skincare routine to a new level. Our powerful formula defends skin against external agents like everyday stress, lack of sleep, and destructive environment. Lightweight, non-greasy face cream for men profoundly hydrates skin and fights wrinkles and signs of aging.',
+    link:'ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMUdKQ1VMRTVKR0hPJmVuY3J5cHRlZElkPUEwMjA2MjA5MVI2Nkk3NllXQU1LMiZlbmNyeXB0ZWRBZElkPUEwMDk2OTkzMUQ1UDRLMEo3OTg3VyZ3aWRnZXROYW1lPXNwX2RldGFpbF90aGVtYXRpYyZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU',
+    image:'https://images-na.ssl-images-amazon.com/images/I/81Pe7G1fSlL._SL1500_.jpg'
+  },
+  {
+    id: 6,
+    name: 'Tiege Hanley Men’s Skin Care System - Level 1 | Mens Routine Product Set: Face Wash, Morning Moisturizer, Exfoliating Face Scrub, Night Moisturizer | Cleanse, Tighten & Hydrate Your Best Looking Skin',
+    price: 39.99,
+    description: 'BASIC SKIN CARE SET FOR MEN: Our level 1 skincare kit includes facial cleanser (face wash), exfoliating face scrub, AM face moisturizer (SPF20) and PM night moisturizer- all in a 30 day supply. Great starter pack for you or a gift for someone else.',
+    link:'https://www.amazon.com/dp/B071NHHX8G/ref=sspa_dk_detail_2?psc=1&pd_rd_i=B071NHHX8G&pd_rd_w=5rNHf&pf_rd_p=cbc856ed-1371-4f23-b89d-d3fb30edf66d&pd_rd_wg=dHXBh&pf_rd_r=RPMVYFZNWWW9K4QAHD0W&pd_rd_r=eb58e8f9-760c-41ae-81da-9ce4bdcdd177&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFHNlQ0Nk9CNUJTSUUmZW5jcnlwdGVkSWQ9QTA0MzczMTgzT0RDSEFJVUozVDNXJmVuY3J5cHRlZEFkSWQ9QTAwMTIyNDYyNDk4WTIwSFBITFFTJndpZGdldE5hbWU9c3BfZGV0YWlsX3RoZW1hdGljJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==',    
+    image:'https://images-na.ssl-images-amazon.com/images/I/61KOKCMiBxL._SL1000_.jpg'
+  },
+  {
+    id: 7,
+    name: 'Brickell Mens Restoring Eye Routine for Men, Eye Serum and Eye Cream for Men, Natural and Organic, Unscented',
+    price: 85.00,
+    description: 'What It Does: This two part eye care routine reduces wrinkles, bags, and dark circles under your eyes for a more youthful, energized complexion.',
+    link:'https://www.amazon.com/dp/B086MJHBSP/ref=sspa_dk_detail_4?psc=1&pd_rd_i=B086MJHBSP&pd_rd_w=bbVuM&pf_rd_p=cbc856ed-1371-4f23-b89d-d3fb30edf66d&pd_rd_wg=EhwKN&pf_rd_r=CBAJZEABME90ZWSKTEZP&pd_rd_r=0bf4b40e-6e7e-40d8-9d47-92f9536118ae&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExN0NCVTk0RDUyODJMJmVuY3J5cHRlZElkPUEwNTY5NjE1MUxCQlcxUlk4QVZWTCZlbmNyeXB0ZWRBZElkPUEwMzU3MTgzMkk1S1ZBQzJWVUhZSCZ3aWRnZXROYW1lPXNwX2RldGFpbF90aGVtYXRpYyZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+    image:'https://images-na.ssl-images-amazon.com/images/I/518C3UsRlUL._SL1050_.jpg'
+  },
+  {
+    id: 8,
+    name: 'Brickell Mens Daily Advanced Face Care Routine I, Gel Facial Cleanser Wash, Face Scrub, Face Moisturizer Lotion, Natural and Organic, Scented',
+    price: 77.00,
+    description: 'What It Does: This men’s skin care set contains the three best skin care products for men to cleanse the face of shine & oil, remove dead skin & impurities, while nourishing, protecting, and hydrating it.',
+    image:'https://images-na.ssl-images-amazon.com/images/I/71jZ3l4NkGL._SL1500_.jpg',
+    link:'https://www.amazon.com/dp/B073T5H2X5/ref=sspa_dk_detail_1?psc=1&pd_rd_i=B073T5H2X5&pd_rd_w=3TnqD&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=C1abf&pf_rd_r=7S1XEH4TDFAPA1Z9WEEX&pd_rd_r=b107fb5a-9836-421d-b5be-1db68ef11e13&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzUDFXRkkxOUZIWFomZW5jcnlwdGVkSWQ9QTA5NTY3MjExQllSMjYwMlU5NzdEJmVuY3J5cHRlZEFkSWQ9QTAwNzgzNzUzRVdGQTZJR0xKNDhRJndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='  },
+  {
+    id: 9,
+    name: 'The Modern Bathroom Collection for Men: 5 Piece Kit to Cleanse/Moisturize your Hair, Skin, and Body - Includes Moisturizing Balm, Charcoal Cleanser, Shampoo,Conditioner, and Body Wash - by Lumin',
+    price: 66.00,
+    description: 'The Modern Bathroom Collection: The perfect gift set for men serious about looking and feeling great: including yourself. Hair-to-toe coverage keeping you fresh and clean. In our Modern Bathroom Set, you’ll receive our Ultra-Hydrating Moisturizing Balm, No-Nonsense Charcoal Cleanser, Keratin Recovery Shampoo, Keratin Strengthening Conditioner, and Clarifying Body Wash.',
+    image:'https://images-na.ssl-images-amazon.com/images/I/81hIgZVqRlL._SL1500_.jpg',
+    link:'https://www.amazon.com/dp/B089VLQLK6/ref=sspa_dk_detail_6?psc=1&pd_rd_i=B089VLQLK6&pd_rd_w=1itRG&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=Ox1Th&pf_rd_r=GB14HBWG5G2BHEMQY2HC&pd_rd_r=62c3fe04-1dd0-4550-933d-2cce3e638438&smid=A2YWS7F246NXNA&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyWVNINzFXSVgwQUFGJmVuY3J5cHRlZElkPUEwNTgyODU4U002QVJJREFIOE4xJmVuY3J5cHRlZEFkSWQ9QTA3NzI5MTYyUU45SkxUNDVQMVFSJndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
+  }
+];
